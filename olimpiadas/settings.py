25c55ab27b4174fc93166608107dca47db10e2b8
rodri-perez <rodri.08gabriel.perez@gmail.com>
2024-08-24 18:55:04 -0300

    csrf

--- olimpiadas/settings.py
+++ olimpiadas/settings.py
@@ -141,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['http://*',
+                        'https://olimpiadas-backend-production.up.railway.app']
