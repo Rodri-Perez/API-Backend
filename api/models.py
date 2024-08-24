13ebad2aeba9930313c363c0e266f27e1f821436
rodri-perez <rodri.08gabriel.perez@gmail.com>
2024-08-24 17:36:12 -0300

    cloud

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Producto(models.Model):
+
+    id_producto = models.UUIDField(
+        primary_key=True, editable=False),
+    cod = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=255)
+    talle_peso = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    stock = models.PositiveIntegerField()
+    precio = models.PositiveIntegerField()
